7b478f778df843051a156dccb3ecbf7a903490f4
Sung-Eun Choi <sungeunchoi@users.noreply.github.com>
2015-01-28 18:23:27 -0600

    Initial GNI stubs.

---
+++ prov/gni/src/gnix_ep.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015 Cray Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+//
+// Endpoint common code
+//
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "gnix.h"
+
+struct fi_info *
+gnix_fi_info(enum fi_ep_type ep_type, struct fi_info *hints)
+{
+  struct fi_info *_info = fi_allocinfo_internal();
+  if (!_info) return NULL;
+
+  // fill in attributes common to all GNI endpoints
+  _info->ep_type = ep_type;
+  _info->ep_attr->protocol = FI_PROTO_UNSPEC; // should we define FI_PROTO_GNI?
+
+  return _info;
+}
+
+
--- prov/gni/src/gnix_init.c
+++ prov/gni/src/gnix_init.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014 Los Alamos National Security, LLC. Allrights reserved.
+ * Copyright (c) 2015 Cray Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -30,12 +31,73 @@
  * SOFTWARE.
  */
 
-#include "gni_pub.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <gni_pub.h>
+#include "gnix.h"
 #include "fi.h"
 #include "prov.h"
 
+// This is useful for making sure we have the gni headers and lib
+// paths right.  Remove when we get some real code in here.
 int gnix_foobar(void){
        uint32_t version;
        GNI_GetVersion(&version);
        return 0;
+}
+
+
+static int
+gnix_getinfo(uint32_t version, const char *node, const char *service,
+             uint64_t flags, struct fi_info *hints, struct fi_info **info)
+{
+  // Do some verification of hints here
+
+  int err = -FI_ENODATA;
+  
+  if (hints) {
+    switch (hints->ep_type) {
+    case FI_EP_RDM:
+      return gnix_rdm_getinfo(version, node, service, flags, hints, info);
+    case FI_EP_DGRAM:
+      return err;
+    case FI_EP_MSG:
+      return err;
+    default:
+      break;
+    }
+  }
+
+  // Chain together info from all supported endpoint types
+  // Currently, we only support RDM
+  err = gnix_rdm_getinfo(version, node, service, flags, hints, info);
+
+  return err;
+}
+
+static int
+gnix_fabric(struct fi_fabric_attr *attr,
+            struct fid_fabric **fabric, void *context)
+{
+  return 0;
+}
+
+static void
+gnix_fini(void)
+{
+  return;
+}
+
+struct fi_provider gnix_prov = {
+  .name = "GNI",
+  .version = FI_VERSION(GNI_MAJOR_VERSION, GNI_MINOR_VERSION), 
+  .fi_version = FI_VERSION(FI_MAJOR_VERSION, FI_MINOR_VERSION),
+  .getinfo = gnix_getinfo,
+  .fabric = gnix_fabric,
+  .cleanup = gnix_fini
+};
+
+GNI_INI
+{
+  return (&gnix_prov);
 }
