ea3fdc356cbd6a4dcfb6332d2c0f1fbf598c7b37
Howard Pritchard <howardp@lanl.gov>
2015-03-11 16:57:50 -0500

    prov/gni: add sanity assert for domain ref_cnt
    add sanity assert in domain ref_cnt  decrement
    in the cq close method.
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- prov/gni/src/gnix_cq.c
+++ prov/gni/src/gnix_cq.c
@@ -36,6 +36,7 @@
  */
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "gnix.h"
 #include "gnix_util.h"
@@ -50,6 +51,7 @@ static int gnix_cq_close(fid_t fid)
 		return -FI_EBUSY;
 
 	atomic_dec(&cq->domain->ref_cnt);
+	assert(&cq->domain->ref_cnt > 0);
 
 #if 0
 	while (!slist_empty(&cq->free_list)) {
