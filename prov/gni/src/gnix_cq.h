6b94c3ac8a7ae0b3c5ae077458fc3e3104208c13
Ben Turrubiates <bturrubiates@lanl.gov>
2015-04-06 14:51:47 -0700

    Implement gnix_fid_cq.
    Restructure cq_entry and gnix_fid_cq.
    - Store entire attr object.
    - Store void * for actual entry.
    
    - Separate into a header file and module file.
    
    TODO: Implement wait objects.
    
    Open now allocates a free list of size addr.size or a default size of
    GNIX_CQ_DEFAULT_SIZE (256).
    
    Signed-off-by: Ben Turrubiates <bturrubiates@lanl.gov>

---
+++ prov/gni/src/gnix_cq.h
@@ -0,0 +1,55 @@
+#ifndef _GNIX_CQ_H_
+#define _GNIX_CQ_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <fi.h>
+#include <fi_list.h>
+
+#define GNIX_CQ_DEFAULT_FORMAT struct fi_cq_entry
+#define GNIX_CQ_DEFAULT_SIZE   256
+
+struct gnix_cq_entry {
+	void *the_entry;
+	fi_addr_t source;
+	struct slist_entry item;
+};
+
+/*
+ * TODO:
+ * - Should we consider separate locks for each queue (EV v. ERR) for concurrent
+ *   access?
+ */
+struct gnix_fid_cq {
+	struct fid_cq cq_fid;
+	struct gnix_fid_domain *domain;
+
+	struct slist ev_queue;
+	struct slist err_queue;
+	struct slist ev_free;
+	struct slist err_free;
+
+	struct fi_cq_attr attr;
+	size_t entry_size;
+
+	fastlock_t lock;
+	atomic_t ref_cnt;
+};
+
+
+ssize_t gnix_cq_add_event(struct gnix_fid_cq *cq, void *op_context,
+			  uint64_t flags, size_t len, void *buf,
+			  uint64_t data, uint64_t tag);
+
+ssize_t gnix_cq_add_error(struct gnix_fid_cq *cq, void *op_context,
+			  uint64_t flags, size_t len, void *buf,
+			  uint64_t data, uint64_t tag, size_t olen,
+			  int err, int prov_errno, void *err_data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
