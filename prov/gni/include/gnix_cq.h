313db30b3e1d42aeb0ee74907ad7b6ad2f5038f8
Ben Turrubiates <bturrubiates@lanl.gov>
2015-04-08 15:14:37 -0700

    Fix naming of prototypes in header file. Fix seg fault arising from passing incorrect pointer to fill functions.
    Signed-off-by: Ben Turrubiates <bturrubiates@lanl.gov>

--- prov/gni/include/gnix_cq.h
+++ prov/gni/include/gnix_cq.h
@@ -39,11 +39,11 @@ struct gnix_fid_cq {
 };
 
 
-ssize_t gnix_cq_add_event(struct gnix_fid_cq *cq, void *op_context,
+ssize_t _gnix_cq_add_event(struct gnix_fid_cq *cq, void *op_context,
 			  uint64_t flags, size_t len, void *buf,
 			  uint64_t data, uint64_t tag);
 
-ssize_t gnix_cq_add_error(struct gnix_fid_cq *cq, void *op_context,
+ssize_t _gnix_cq_add_error(struct gnix_fid_cq *cq, void *op_context,
 			  uint64_t flags, size_t len, void *buf,
 			  uint64_t data, uint64_t tag, size_t olen,
 			  int err, int prov_errno, void *err_data);
