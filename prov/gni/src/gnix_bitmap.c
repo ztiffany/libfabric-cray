ff2d46c98276bc9ec3a9d61a6a0041c0edf4efba
James Swaro <jswaro@cray.com>
2015-04-24 15:34:30 -0500

    Initial bitmap implementation
    This commit introduces a new structure for handling bitmaps in the
    gni provider.
    
    The gnix_bitmap_t provides a set of basic functions for interacting
    with the bitmap as listed below:
    -- alloc_bitmap
    -- realloc_bitmap
    -- free_bitmap
    -- test_bit
    -- set_bit
    -- clear_bit
    -- test_and_set_bit
    -- test_and_clear_bit
    -- find_first_set_bit
    -- find_first_clear_bit
    -- bitmap_empty
    -- bitmap_full

---
+++ prov/gni/src/gnix_bitmap.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2015 Cray Inc. All rights reserved.
+ *
+ *  Created on: Apr 16, 2015
+ *      Author: jswaro
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+#include "gnix_bitmap.h"
+
+int find_first_zero_bit(gnix_bitmap_t *bitmap)
+{
+	int i, pos;
+	gnix_bitmap_value_t value;
+
+	for (i = 0, pos = 0;
+			i < GNIX_BITMAP_BLOCKS(bitmap->length);
+			++i, pos += GNIX_BITMAP_BUCKET_LENGTH) {
+		/* invert the bits to check for first zero bit */
+		value = ~(__gnix_load_block(bitmap, i));
+
+		if (value != 0) {
+			/* no need to check for errors because we have
+			   established there is an unset bit */
+			pos += ffsll(value) - 1;
+
+			return pos;
+		}
+	}
+
+	return -EAGAIN;
+}
+
+int find_first_set_bit(gnix_bitmap_t *bitmap)
+{
+	int i, pos;
+	gnix_bitmap_value_t value;
+
+	for (i = 0, pos = 0;
+			i < GNIX_BITMAP_BLOCKS(bitmap->length);
+			++i, pos += GNIX_BITMAP_BUCKET_LENGTH) {
+		value = __gnix_load_block(bitmap, i);
+
+		if (value != 0) {
+			/* no need to check for errors because we have
+			   established there is a set bit */
+			pos += ffsll(value) - 1;
+
+			return pos;
+		}
+	}
+
+	return -EAGAIN;
+}
+
+void fill_bitmap(gnix_bitmap_t *bitmap, uint64_t value)
+{
+	int i;
+	gnix_bitmap_value_t fill_value = (value != 0) ? ~0 : 0;
+
+	for (i = 0; i < GNIX_BITMAP_BLOCKS(bitmap->length); ++i) {
+		__gnix_set_block(bitmap, i, fill_value);
+	}
+}
+
+int alloc_bitmap(gnix_bitmap_t *bitmap, uint32_t nbits)
+{
+	int i;
+
+	if (bitmap->state == GNIX_BITMAP_STATE_READY)
+		return -EINVAL;
+
+	if (bitmap->length != 0 || nbits == 0)
+		return -EINVAL;
+
+	bitmap->arr = calloc(GNIX_BITMAP_BLOCKS(nbits),
+			sizeof(gnix_bitmap_block_t));
+	if (!bitmap->arr)
+		return -ENOMEM;
+
+	bitmap->length = nbits;
+
+	for (i = 0; i < GNIX_BITMAP_BLOCKS(bitmap->length); ++i)
+		__gnix_init_block(&bitmap->arr[i]);
+
+	bitmap->state = GNIX_BITMAP_STATE_READY;
+
+	return 0;
+}
+
+int realloc_bitmap(gnix_bitmap_t *bitmap, uint32_t nbits)
+{
+	gnix_bitmap_block_t *new_allocation;
+	int blocks_to_allocate = GNIX_BITMAP_BLOCKS(nbits);
+	int i;
+
+	if (bitmap->state != GNIX_BITMAP_STATE_READY)
+		return -EINVAL;
+
+	if (nbits == 0 || bitmap->arr == NULL)
+		return -EINVAL;
+
+	new_allocation = realloc(bitmap->arr,
+			(blocks_to_allocate *
+					sizeof(gnix_bitmap_block_t)));
+
+	if (!new_allocation)
+		return -ENOMEM;
+
+	bitmap->arr = new_allocation;
+
+	/* Did we increase the size of the bitmap?
+	 * If so, initialize new blocks */
+	if (blocks_to_allocate > GNIX_BITMAP_BLOCKS(bitmap->length)) {
+		for (i = GNIX_BITMAP_BLOCKS(bitmap->length);
+				i < blocks_to_allocate;
+				++i) {
+			__gnix_init_block(&bitmap->arr[i]);
+		}
+	}
+
+	bitmap->length = nbits;
+
+	return 0;
+}
+
+int free_bitmap(gnix_bitmap_t *bitmap)
+{
+	if (bitmap->state != GNIX_BITMAP_STATE_READY)
+		return -EINVAL;
+
+	bitmap->length = 0;
+	if (bitmap->arr) {
+		free(bitmap->arr);
+		bitmap->arr = NULL;
+	}
+
+	bitmap->state = GNIX_BITMAP_STATE_FREE;
+
+	return 0;
+}
+
---
+++ prov/gni/test/bitmap.c
@@ -0,0 +1,499 @@
+/*
+ * Copyright (c) 2015 Cray Inc. All rights reserved.
+ *
+ *  Created on: Apr 23, 2015
+ *      Author: jswaro
+ */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <errno.h>
+#include <gnix_bitmap.h>
+
+#ifdef assert
+#undef assert
+#endif
+
+#include <criterion/criterion.h>
+
+gnix_bitmap_t *test_bitmap = NULL;
+int call_free_bitmap = 0;
+
+void calculate_time_difference(struct timeval *start, struct timeval *end,
+		int *secs_out, int *usec_out)
+{
+	*secs_out = end->tv_sec - start->tv_sec;
+	if (end->tv_usec < start->tv_usec) {
+		*secs_out = *secs_out - 1;
+		*usec_out = (1000000 + end->tv_usec) - start->tv_usec;
+	} else {
+		*usec_out = end->tv_usec - start->tv_usec;
+	}
+}
+
+void __gnix_bitmap_test_setup(void)
+{
+	assert(test_bitmap == NULL);
+	test_bitmap = (gnix_bitmap_t *) calloc(1, sizeof(test_bitmap));
+	memset(test_bitmap, 0, sizeof(gnix_bitmap_t));
+	assert(test_bitmap != NULL);
+
+	call_free_bitmap = 1;
+}
+
+void __gnix_bitmap_test_teardown(void)
+{
+	if (call_free_bitmap) {
+		free_bitmap(test_bitmap);
+	} else if (test_bitmap && test_bitmap->arr) {
+		free(test_bitmap->arr);
+	}
+
+	assert(test_bitmap != NULL);
+	free(test_bitmap);
+	test_bitmap = NULL;
+}
+
+
+static void __test_clean_bitmap_state(gnix_bitmap_t *bitmap,
+		int _length, gnix_bitmap_state_e _state)
+{
+	assert(bitmap->arr != NULL);
+	assert(bitmap->length == _length);
+	assert(bitmap->state == _state);
+}
+
+static void __test_initialize_bitmap(gnix_bitmap_t *bitmap, int bits)
+{
+	int ret = alloc_bitmap(bitmap, bits);
+
+	assert(ret == 0);
+	__test_clean_bitmap_state(bitmap, bits, GNIX_BITMAP_STATE_READY);
+}
+
+static void __test_initialize_bitmap_clean(gnix_bitmap_t *bitmap, int bits)
+{
+	__test_initialize_bitmap(bitmap, bits);
+	assert(bitmap_empty(bitmap));
+}
+
+static void __test_realloc_bitmap(gnix_bitmap_t *bitmap, int bits)
+{
+	int ret = realloc_bitmap(bitmap, bits);
+
+	assert(ret == 0);
+	__test_clean_bitmap_state(bitmap, bits,	GNIX_BITMAP_STATE_READY);
+}
+
+static void __test_realloc_bitmap_clean(gnix_bitmap_t *bitmap, int initial,
+		int next)
+{
+	__test_initialize_bitmap(bitmap, initial);
+	__test_realloc_bitmap(bitmap, next);
+	assert(bitmap_empty(bitmap));
+}
+
+static void __test_free_bitmap_clean(gnix_bitmap_t *bitmap)
+{
+	int ret = free_bitmap(bitmap);
+
+	assert(ret == 0);
+	assert(bitmap->arr == NULL);
+	assert(bitmap->length == 0);
+	assert(bitmap->state == GNIX_BITMAP_STATE_FREE);
+}
+
+/*
+ * Basic functionality tests for the gnix_bitmap_t object
+ */
+
+TestSuite(gnix_bitmap,
+		.init = __gnix_bitmap_test_setup,
+		.fini = __gnix_bitmap_test_teardown);
+
+Test(gnix_bitmap, uninitialized)
+{
+	assert(test_bitmap->arr == NULL);
+	assert(test_bitmap->length == 0);
+	assert(test_bitmap->state == GNIX_BITMAP_STATE_UNINITIALIZED);
+
+	call_free_bitmap = 0;
+}
+
+Test(gnix_bitmap, initialize_128)
+{
+	__test_initialize_bitmap(test_bitmap, 128);
+
+	call_free_bitmap = 0;
+}
+
+Test(gnix_bitmap, initialize_1)
+{
+	__test_initialize_bitmap(test_bitmap, 1);
+
+	call_free_bitmap = 0;
+}
+
+Test(gnix_bitmap, initialize_0)
+{
+	int ret;
+
+	ret = alloc_bitmap(test_bitmap, 0);
+	assert(ret == -EINVAL);
+
+	call_free_bitmap = 0;
+}
+
+Test(gnix_bitmap, already_initialized)
+{
+	int ret;
+
+	__test_initialize_bitmap(test_bitmap, 128);
+
+	ret = alloc_bitmap(test_bitmap, 128);
+	assert(ret == -EINVAL);
+
+	call_free_bitmap = 0;
+}
+
+Test(gnix_bitmap, destroy_bitmap)
+{
+	__test_initialize_bitmap(test_bitmap, 128);
+
+	__test_free_bitmap_clean(test_bitmap);
+}
+
+Test(gnix_bitmap, destroy_bitmap_uninitialized)
+{
+	int ret;
+
+	ret = free_bitmap(test_bitmap);
+	assert(ret == -EINVAL);
+	expect(test_bitmap->arr == NULL);
+	expect(test_bitmap->length == 0);
+	expect(test_bitmap->state == GNIX_BITMAP_STATE_UNINITIALIZED);
+}
+
+Test(gnix_bitmap, destroy_bitmap_already_freed)
+{
+	int ret;
+
+	__test_initialize_bitmap(test_bitmap, 128);
+
+	__test_free_bitmap_clean(test_bitmap);
+
+	ret = free_bitmap(test_bitmap);
+	assert(ret == -EINVAL);
+	expect(test_bitmap->arr == NULL);
+	expect(test_bitmap->length == 0);
+	expect(test_bitmap->state == GNIX_BITMAP_STATE_FREE);
+}
+
+Test(gnix_bitmap, realloc_63)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 63);
+}
+
+Test(gnix_bitmap, realloc_64)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 64);
+}
+
+Test(gnix_bitmap, realloc_65)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 65);
+}
+
+Test(gnix_bitmap, realloc_255)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 255);
+}
+
+Test(gnix_bitmap, realloc_256)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 256);
+}
+
+Test(gnix_bitmap, realloc_257)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 257);
+}
+
+Test(gnix_bitmap, realloc_63_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 63);
+}
+
+Test(gnix_bitmap, realloc_64_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 64);
+}
+
+Test(gnix_bitmap, realloc_65_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 65);
+}
+
+Test(gnix_bitmap, realloc_255_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 255);
+}
+
+Test(gnix_bitmap, realloc_256_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 256);
+}
+
+Test(gnix_bitmap, realloc_257_check_bits)
+{
+	__test_realloc_bitmap_clean(test_bitmap, 128, 257);
+}
+
+Test(gnix_bitmap, bit_set_test_pass)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+
+	assert(test_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_set_test_fail)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+
+	assert(!test_bit(test_bitmap, 0));
+}
+
+Test(gnix_bitmap, bit_set_clear)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+
+	assert(test_bit(test_bitmap, 1));
+
+	clear_bit(test_bitmap, 1);
+
+	assert(!test_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_clear)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	clear_bit(test_bitmap, 1);
+
+	assert(!test_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_set)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+}
+
+Test(gnix_bitmap, bit_test_and_set_unset)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	assert(!test_and_set_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_test_and_set_already_set)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+	assert(test_bit(test_bitmap, 1));
+
+	assert(test_and_set_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_test_and_clear_unset)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	assert(!test_and_clear_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, bit_test_and_clear_already_set)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 1);
+	assert(test_bit(test_bitmap, 1));
+
+	assert(test_and_clear_bit(test_bitmap, 1));
+}
+
+Test(gnix_bitmap, ffs_clean_bitmap)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	assert(find_first_set_bit(test_bitmap) == -EAGAIN);
+}
+
+Test(gnix_bitmap, ffs_first_bit_set)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 0);
+
+	assert(find_first_set_bit(test_bitmap) == 0);
+}
+
+Test(gnix_bitmap, ffs_seventeen_set)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	set_bit(test_bitmap, 17);
+
+	assert(find_first_set_bit(test_bitmap) == 17);
+}
+
+Test(gnix_bitmap, ffz_clean_bitmap)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	assert(find_first_zero_bit(test_bitmap) == 0);
+}
+
+Test(gnix_bitmap, ffz_full_bitmap)
+{
+	int i;
+
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	for (i = 0; i < test_bitmap->length; ++i) {
+		set_bit(test_bitmap, i);
+		assert(test_bit(test_bitmap, i));
+	}
+
+	assert(find_first_zero_bit(test_bitmap) == -EAGAIN);
+}
+
+Test(gnix_bitmap, ffz_first_half_set)
+{
+	int i;
+
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	for (i = 0; i < 32 ; ++i) {
+		set_bit(test_bitmap, i);
+		assert(test_bit(test_bitmap, i));
+	}
+
+	expect(test_bitmap->length == 64);
+	expect(i == 32);
+	assert(find_first_zero_bit(test_bitmap) == i);
+}
+
+Test(gnix_bitmap, map_fill_0)
+{
+	int i;
+
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	for (i = 0; i < test_bitmap->length; ++i) {
+		set_bit(test_bitmap, i);
+		assert(test_bit(test_bitmap, i));
+	}
+
+	assert(bitmap_full(test_bitmap));
+
+	fill_bitmap(test_bitmap, 0);
+
+	assert(bitmap_empty(test_bitmap));
+}
+
+Test(gnix_bitmap, map_fill_1)
+{
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	fill_bitmap(test_bitmap, 1);
+
+	assert(bitmap_full(test_bitmap));
+}
+
+Test(gnix_bitmap, bitmap_load)
+{
+	gnix_bitmap_value_t expected = ~0;
+
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	fill_bitmap(test_bitmap, 1);
+
+	assert(expected == __gnix_load_block(test_bitmap, 0));
+}
+
+Test(gnix_bitmap, bitmap_set)
+{
+	gnix_bitmap_value_t expected = ~0;
+
+	__test_initialize_bitmap_clean(test_bitmap, 64);
+
+	__gnix_set_block(test_bitmap, 0, expected);
+
+	assert(__gnix_load_block(test_bitmap, 0) == expected);
+}
+
+
+Test(gnix_bitmap, performance_set_test)
+{
+	int i, j;
+	int secs, usec;
+	struct timeval start, end;
+
+	__test_initialize_bitmap_clean(test_bitmap, 8192);
+
+	gettimeofday(&start, 0);
+	for (i = 0; i < 100000; ++i) {
+		j = i % 8192;
+		set_bit(test_bitmap, j);
+		assert(test_bit(test_bitmap, j));
+		clear_bit(test_bitmap, j);
+		assert(!test_bit(test_bitmap, j));
+	}
+	gettimeofday(&end, 0);
+
+	calculate_time_difference(&start, &end, &secs, &usec);
+
+	assert(bitmap_empty(test_bitmap));
+
+	expect(secs < 1);
+}
+
+Test(gnix_bitmap, performance_set_test_random)
+{
+	int i, j;
+	int secs, usec;
+	struct timeval start, end;
+
+	srand(time(NULL));
+
+	__test_initialize_bitmap_clean(test_bitmap, 8192);
+
+	gettimeofday(&start, 0);
+	for (i = 0; i < 100000; ++i) {
+		j = rand() % 8192;
+		set_bit(test_bitmap, j);
+		assert(test_bit(test_bitmap, j));
+		clear_bit(test_bitmap, j);
+		assert(!test_bit(test_bitmap, j));
+	}
+	gettimeofday(&end, 0);
+
+	calculate_time_difference(&start, &end, &secs, &usec);
+
+	assert(bitmap_empty(test_bitmap));
+
+	expect(secs < 1);
+}
+
