7f186b1b04e4b17d682f3ea1fcdcedfd19926046
James Swaro <jswaro@cray.com>
2015-05-12 13:23:38 -0500

    Adding structure changes

--- prov/gni/include/gnix_mr.h
+++ prov/gni/include/gnix_mr.h
@@ -21,12 +21,14 @@ typedef struct gnix_mr {
 } gnix_mr_t;
 
 #define GNIX_MR_PAGE_SHIFT 12
-#define GNIX_MR_VA_BITS 37
+#define GNIX_MR_PFN_BITS 37
 #define GNIX_MR_MDD_BITS GNIX_MR_PAGE_SHIFT
+#define GNIX_MR_FMT_BITS 1
 #define GNIX_MR_FLAG_BITS 1
-#define GNIX_MR_PFN_BITS (GNIX_MR_VA_BITS + GNIX_MR_MDD_BITS)
-#define GNIX_MR_KEY_BITS (GNIX_MR_PFN_BITS + GNIX_MR_FLAG_BITS)
-#define GNIX_MR_PADDING_LENGTH (64 - GNIX_MR_KEY_BITS)
+#define GNIX_MR_KEY_BITS (GNIX_MR_PFN_BITS + GNIX_MR_MDD_BITS)
+#define GNIX_MR_RESERVED_BITS \
+	(GNIX_MR_KEY_BITS + GNIX_MR_FLAG_BITS + GNIX_MR_FMT_BITS)
+#define GNIX_MR_PADDING_LENGTH (64 - GNIX_MR_RESERVED_BITS)
 
 enum {
 	GNIX_MR_FLAG_READONLY = 1 << 0
@@ -35,13 +37,11 @@ enum {
 typedef struct gnix_mr_key {
 	union {
 		struct {
-			union  {
-				uint64_t pfn : GNIX_MR_PFN_BITS;
-				struct {
-					uint64_t va: GNIX_MR_VA_BITS;
-					uint64_t mdd: GNIX_MR_MDD_BITS;
-				};
+			struct {
+				uint64_t pfn: GNIX_MR_PFN_BITS;
+				uint64_t mdd: GNIX_MR_MDD_BITS;
 			};
+			uint64_t format : GNIX_MR_FMT_BITS;
 			uint64_t flags : GNIX_MR_FLAG_BITS;
 			uint64_t padding: GNIX_MR_PADDING_LENGTH;
 		};
