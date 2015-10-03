5d1704e51f1193864e1a10d75d8f998efe597d29
Sung-Eun Choi <sungeunchoi@users.noreply.github.com>
2015-10-02 19:01:25 -0500

    Small changes to nic progress (mostly from Howard):
    - fix overrun code
    - update log type
    
    Signed-off-by: Sung-Eun Choi <sungeunchoi@users.noreply.github.com>

--- prov/gni/src/gnix_nic.c
+++ prov/gni/src/gnix_nic.c
@@ -72,6 +72,8 @@ static int __nic_rx_overrun(struct gnix_nic *nic)
 	gni_return_t status;
 	gni_cq_entry_t cqe;
 
+	GNIX_WARN(FI_LOG_EP_DATA, "\n");
+
 	/* clear out the CQ */
 	while ((status = GNI_CqGetEvent(nic->rx_cq, &cqe)) == GNI_RC_SUCCESS);
 	assert(status == GNI_RC_NOT_DONE);
@@ -89,8 +91,15 @@ static int __nic_rx_overrun(struct gnix_nic *nic)
 		ret = _gnix_test_bit(&nic->vc_id_bitmap, i);
 		if (ret) {
 			vc = __gnix_nic_elem_by_rem_id(nic, i);
-			ret = _gnix_vc_schedule(vc);
-			assert(ret == FI_SUCCESS);
+			if (unlikely(vc->conn_state != GNIX_VC_CONNECTED))
+				_gnix_set_bit(&vc->flags,
+					      GNIX_VC_FLAG_RX_PENDING);
+			ret = _gnix_vc_dequeue_smsg(vc);
+			if (ret != FI_SUCCESS) {
+				GNIX_WARN(FI_LOG_EP_DATA,
+					  "_gnix_vc_dqueue_smsg returned %d\n",
+					  ret);
+			}
 		}
 	}
 
@@ -107,19 +116,19 @@ static int process_rx_cqe(struct gnix_nic *nic, gni_cq_entry_t cqe)
 
 #if 1 /* Process RX inline with arrival of an RX CQE. */
 	if (unlikely(vc->conn_state != GNIX_VC_CONNECTED)) {
-		GNIX_INFO(FI_LOG_EP_CTRL,
+		GNIX_INFO(FI_LOG_EP_DATA,
 			  "Scheduling VC for RX processing (%p)\n",
 			  vc);
 		_gnix_set_bit(&vc->flags, GNIX_VC_FLAG_RX_PENDING);
 		ret = _gnix_vc_schedule(vc);
 		assert(ret == FI_SUCCESS);
 	} else {
-		GNIX_INFO(FI_LOG_EP_CTRL,
+		GNIX_INFO(FI_LOG_EP_DATA,
 			  "Processing VC RX (%p)\n",
 			  vc);
 		ret = _gnix_vc_dequeue_smsg(vc);
 		if (ret != FI_SUCCESS) {
-			GNIX_WARN(FI_LOG_EP_CTRL,
+			GNIX_WARN(FI_LOG_EP_DATA,
 					"_gnix_vc_dqueue_smsg returned %d\n",
 					ret);
 		}
@@ -300,7 +309,7 @@ int __nic_vc_progress(struct gnix_nic *nic)
 	while ((vc = _gnix_nic_next_pending_vc(nic))) {
 		ret = _gnix_vc_progress(vc);
 		if (ret != FI_SUCCESS) {
-			GNIX_INFO(FI_LOG_EP_CTRL,
+			GNIX_INFO(FI_LOG_EP_DATA,
 				  "Rescheduling VC (%p)\n", vc);
 			ret = _gnix_vc_schedule(vc);
 			assert(ret == FI_SUCCESS);
