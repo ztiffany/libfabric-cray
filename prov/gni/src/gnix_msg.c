6e07a876f4c01448d650b019f0e260a2bd0fd6fa
Sung-Eun Choi <sungeunchoi@users.noreply.github.com>
2015-11-17 19:51:43 -0600

    Silence compiler warning when calling free().
    Signed-off-by: Sung-Eun Choi <sungeunchoi@users.noreply.github.com>

--- prov/gni/src/gnix_msg.c
+++ prov/gni/src/gnix_msg.c
@@ -959,7 +959,7 @@ static int  __gnix_discard_request(struct gnix_fid_ep *ep,
 					addr, req->msg.imm, tag);
 
 		/* data has already been delivered, so just discard it */
-		free(req->msg.recv_addr);
+		free((void *) req->msg.recv_addr);
 		_gnix_fr_free(ep, req);
 	}
 
